d2dbd74043be32e64e07e1fd783e4a3b0184c712
Weibo_Pan <fzzalan@gmail.com>
2016-03-13 10:51:23 +0000

    bcm1

--- test.c
+++ test.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include <bcm2835.h>
 
+#define PIN_LCD_LIGHT RPI_GPIO_P1_26
+
 int main(int argc, char*argv[]){
-	printf("Hello World!");
+	printf("Hello World!\n");
 
+	printf("Initializing bcm2835...\n");
+	if (!bcm2835_init()){
+		printf(" *** Error: *** bcm2835 initialization failed ***");
+	}
 
+	bcm2835_gpio_fsel(PIN_PI_RDY, BCM2835_GPIO_FSEL_OUTP);
+	bcm2835_gpio_write(PIN_PI_RDY, LOW);
+	
+	while(1){
+		bcm2835_delay(1000);	
+		bcm2835_gpio_write(PIN_LCD_LIGHT, HIGH);
+		bcm2835_delay(1000);
+		bcm2835_gpio_write(PIN_LCD_LIGHT, LOW);
+	}
 }
